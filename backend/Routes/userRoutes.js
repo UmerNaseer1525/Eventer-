f5a3d637b55de002c3587182347e94161990a045
UmerNaseer1525 <umernaseer1525@gmail.com>
2026-04-02 20:01:31 +0500

    payments

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../Controller/userController");
 const { authenticateToken } = require("../Middleware/authMiddleware");
-const upload = require("../Middleware/uploadMiddleware");
 
 // Public routes
-router.post("/", upload.single("profileImage"), userController.createUser); // Register
+router.post("/", userController.createUser); // Register
 router.post("/login", userController.loginUser); // Login
 
 // Protected routes - require authentication
@@ -15,7 +14,6 @@ router.delete("/:email", authenticateToken, userController.deleteUser);
 router.put(
   "/:email",
   authenticateToken,
-  upload.single("profileImage"),
   userController.updateUser,
 );
 router.put("/:email/status", authenticateToken, userController.updateStatus);
